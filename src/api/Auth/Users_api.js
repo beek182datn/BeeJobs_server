a9fbec68e3d184ed0103d935838fe94538716b60
hmdung26 <hmdung26@gmail.com>
2024-06-04 00:27:55 +0700

    Create checkJWT fun

--- src/api/Auth/Users_api.js
+++ src/api/Auth/Users_api.js
@@ -2,7 +2,7 @@ var userMD = require("../../model/Users");
 var roleMD = require("../../model/Roles");
 var userRoleMD = require("../../model/Users_Roles");
 
-var {jwtMiddleware,createJWT} = require("../../middleware/JWT");
+var {jwtMiddleware,createJWT,checkJWT} = require("../../middleware/JWT");
 var { sendOtp,verifyOtp} = require("../../middleware/MailerSevice");
 const { MAIL_TYPE } = require("../../config/Mailer_Config");
 
@@ -34,9 +34,10 @@ exports.api_Login = async (req,res,next) => {
                  
                     token.UserInfo = objU
 
-                    let objUserRole = await userRoleMD.UserRoleModel.findOne({IdUser: objU._id})
+                    let objUserRole = await userRoleMD.UserRoleModel.findOne({id_User: objU._id})
+                    console.log(objUserRole);
             if(objUserRole){
-               let objRole = await roleMD.RoleModel.findOne({_id: objUserRole.IdRole})
+               let objRole = await roleMD.RoleModel.findOne({_id: objUserRole.id_Role})
               console.log(objRole);
                 token.Role = objRole.Name;
                 req.Role = objRole.Code;
@@ -117,7 +118,7 @@ exports.api_SignUp = async (req, res, next) => {
   
             await objU.save();
 
-            let objRole = await roleMD.RoleModel.findOne({Code: "ADMIN"})
+            let objRole = await roleMD.RoleModel.findOne({Code: " "})
             if(objRole){
                 let objUserRole = new userRoleMD.UserRoleModel();
 
@@ -154,15 +155,10 @@ if(req.method == "POST"){
 
     }
 
-    const [endcodedHeader, encodedPayload, tokensignature] = tokenAuth.split(".")
-    const tokenData = `${endcodedHeader}.${encodedPayload}`
-    const newsignature= createJWT(tokenData);
-    console.log('Signature from token:', tokensignature);
-    console.log('Newly generated signature:', newsignature);
-    console.log('Signatures match:', newsignature === tokensignature);
-    if (newsignature=== tokensignature) {
-      const payload = JSON.parse(atob(encodedPayload));
-      const user = await userMD.userModel.findOne({_id: payload.sub});
+    let tokencheck =  await checkJWT(tokenAuth);
+    if (tokencheck) {
+   console.log(tokencheck);
+      const user = await userMD.userModel.findOne({_id: tokencheck.sub});
       token.UserInfo = user;
       objReturn.token = token;
       
@@ -180,11 +176,28 @@ res.json(objReturn)
 
 exports.api_verifyOtp = async (req,res,next)=> {
   if(req.method == "POST"){
-    const {email,otp} = req.body;
-console.log(req.body);
-    const isValid = await verifyOtp(email, otp,MAIL_TYPE.OTP_SignUp);
-    console.log(isValid)
+    const {email,otp, type} = req.body;
+
+    if(type == MAIL_TYPE.OTP_SignUp){
+    let isValid = await verifyOtp(email, otp,MAIL_TYPE.OTP_SignUp);
     if (isValid) {
+      objReturn.status = 1;
+      objReturn.msg= "Xác thực thành công";
+     
+    }else {
+      objReturn.status = 1;
+      objReturn.msg= "Xác thực thất bại";
+     
+    }
+    console.log(isValid)
+
+  }else if(type == MAIL_TYPE.OTP_SignUp){
+
+let isValid = await verifyOtp(email, otp,MAIL_TYPE.OTP_FogotPassword);
+    if (isValid) {
+      return res.json()
+
+
       objReturn.status = 1;
       objReturn.msg= "Xác thực thành công";
      
@@ -195,5 +208,18 @@ console.log(req.body);
     }
 
   }
+    
+
+  }
   res.json(objReturn)
 }
+
+exports.api_FogotPasswords = (req,res) => {
+if (req.method == 'POST') {
+  try {
+    const {email} = req.body;
+  } catch (error) {
+    console.log(error)
+  }
+}
+};
