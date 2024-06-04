4d1b88ef6a3105643a15d6963bfc14a099e9ca94
ADMIN\Admin <hmdung26@gmail.com>
2024-06-04 16:47:05 +0700

    update code_auth_api

--- src/api/Auth/Users_api.js
+++ src/api/Auth/Users_api.js
@@ -11,74 +11,71 @@ var objReturn = {
   status: 1,
   msg: " ",
   token: " ",
+  user_info:" "
 };
-var token = {
-  UserInfo: "",
+var user_info = {
+  
   Role: "",
 }
+
+
+
 exports.api_Login = async (req, res, next) => {
   if (req.method == "POST") {
     const { passwd, username } = req.body;
     console.log(req.body);
-   
-  
+
     try {
-
-
-      let objU = await userMD.userModel.findOne({
+      const objU = await userMD.userModel.findOne({
         $or: [{ accout_name: username }, { email: username }]
       });
       console.log(objU);
+
       if (objU !== null) {
-        
         const isPasswordMatch = await bcrypt.compare(passwd, objU.hash_pass);
-
         if (isPasswordMatch) {
+          const userInfo = {
+            // ...objU._doc, // Sao chép tất cả thuộc tính của objU vào userInfo
+            id_user : objU._id,
+            Username: objU.accout_name,
+            Role: null // Khởi tạo Role ban đầu là null
+          };
 
-          token.UserInfo = objU
-
-          let objUserRole = await userRoleMD.UserRoleModel.findOne({ id_User: objU._id })
-          console.log(objUserRole);
+          let objUserRole = await userRoleMD.UserRoleModel.findOne({ id_User: objU._id });
           if (objUserRole) {
-            let objRole = await roleMD.RoleModel.findOne({ _id: objUserRole.id_Role })
+            let objRole = await roleMD.RoleModel.findOne({ _id: objUserRole.id_Role });
             console.log(objRole);
-            token.Role = objRole.Code;
+            userInfo.Role = objRole.Code; // Gán giá trị Role vào userInfo
             req.Role = objRole.Code;
           }
 
           req.user = objU;
-
-          // Gọi jwtMiddleware để tạo JWT
           jwtMiddleware(req, res, () => {
-            // Gửi token về cho client
             objReturn.token = req.token;
           });
-          console.log("Đăng Nhập vào tk:");
-          objReturn.status = 0;
-          objReturn.msg = "Đăng nhập thành công";
 
+          objReturn.user_info = userInfo; // Gán userInfo vào objReturn
+          console.log("Đăng Nhập vào tk:");
+          objReturn.status = 200;
+          objReturn.msg = "Đăng nhập thành công";
         } else {
           objReturn.msg = "Sai Mật Khẩu";
-          objReturn.status = 1;
+          objReturn.status = 400;
           console.log("Sai mật khẩu");
           console.log(objU);
         }
       } else {
-        objReturn.msg =
-          "Không có thông tin người dùng " +
-          accout_name;
+        objReturn.msg = "Không có thông tin người dùng " + accout_name;
         objReturn.info_user = "";
-        objReturn.status = 1;
+        objReturn.status = 400;
       }
     } catch (error) {
       objReturn.msg = "Lỗi : " + error.message;
       console.log(error);
     }
-
-
   }
-  res.json(objReturn);
 
+  res.json(objReturn);
 };
 
 exports.api_SignUp = async (req, res, next) => {
@@ -94,7 +91,7 @@ exports.api_SignUp = async (req, res, next) => {
     if (email != null && passwd != null && accout_name != null) {
       if (objU != null) {
         objReturn.msg = "Tài Khoản này đã được đăng ký";
-        objReturn.status = 3;
+        objReturn.status = 400;
         console.log("Tài khoản trùng");
       } else {
         try {
@@ -154,7 +151,7 @@ exports.api_getInfo = async (req, res, next) => {
         console.log(tokencheck);
         const user = await userMD.userModel.findOne({ _id: tokencheck.payload.sub });
         token.UserInfo = user;
-        token.Role = tokencheck.Role;
+        user_info.Role = tokencheck.payload.Role;
         objReturn.token = token;
 
 
@@ -176,11 +173,11 @@ exports.api_verifyOtp = async (req, res, next) => {
     if (type == MAIL_TYPE.OTP_SignUp) {
       let isValid = await verifyOtp(email, otp, MAIL_TYPE.OTP_SignUp);
       if (isValid) {
-        objReturn.status = 1;
+        objReturn.status = 200;
         objReturn.msg = "Xác thực thành công";
 
       } else {
-        objReturn.status = 1;
+        objReturn.status = 400;
         objReturn.msg = "Xác thực thất bại";
 
       }
@@ -190,14 +187,15 @@ exports.api_verifyOtp = async (req, res, next) => {
 
       let isValid = await verifyOtp(email, otp, MAIL_TYPE.OTP_FogotPassword);
       if (isValid) {
-        return res.json()
-
-
         objReturn.status = 1;
         objReturn.msg = "Xác thực thành công";
+   
+
+
+       
 
       } else {
-        objReturn.status = 1;
+        objReturn.status = 400;
         objReturn.msg = "Xác thực thất bại";
 
       }
