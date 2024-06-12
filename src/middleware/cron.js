c465e994fbd0ae7340d9aff3c1bbc07ce9d39893
ADMIN\Admin <hmdung26@gmail.com>
2024-06-12 09:25:05 +0700

    update veryfi

--- src/middleware/cron.js
+++ src/middleware/cron.js
@@ -6,14 +6,14 @@ const User = require('../model/Users');
 const deleteUnverifiedUsers = async () => {
     try {
         // Tính toán thời gian hiện tại trừ đi 1 phút
-        const oneMinuteAgo = new Date(Date.now() - 1 * 60 * 1000);
+        const oneMinuteAgo = new Date(Date.now() - 5 * 60 * 1000);
 
         // Tìm và lưu thông tin người dùng chưa xác thực sau 1 phút
         const unverifiedUsers = await User.userModel.find({ verify: false, create_at: { $lt: oneMinuteAgo } });
 
         // Nếu có người dùng bị xóa, log thông tin của họ
         if (unverifiedUsers.length > 0) {
-            console.log('Deleting unverified users older than 1 minute:');
+            console.log('Deleting unverified users older than 5 minute:');
             unverifiedUsers.forEach(user => {
                 console.log(`User: ${user.accout_name}, Email: ${user.email}`);
             });
@@ -21,7 +21,7 @@ const deleteUnverifiedUsers = async () => {
             // Xóa người dùng chưa xác thực sau 1 phút
             await User.userModel.deleteMany({ _id: { $in: unverifiedUsers.map(user => user._id) } });
         } else {
-            console.log('No unverified users older than 1 minute found.');
+            console.log('No unverified users older than 5 minute found.');
         }
     } catch (error) {
         console.error('Error while deleting unverified users:', error);
