f647c73cb9b82a81c16add141327a8696ece3023
ADMIN\Admin <hmdung26@gmail.com>
2024-08-16 16:06:51 +0700

    uipdate

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,6 @@
 const firebase = require("firebase-admin")
 
-const serviceAccount = require("./serviceAccountKeys.json")
+const serviceAccount = require("./beejobs-ee60a-firebase-adminsdk-j4hbc-49478aa2a5.json")
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
