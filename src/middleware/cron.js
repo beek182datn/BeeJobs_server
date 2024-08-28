a765efbc7d33db2ba10ccfbf9ada25416854e942
DongBeer <dongnguyen2802nb@gmail.com>
2024-08-28 09:34:05 +0700

    update stt job

--- src/middleware/cron.js
+++ src/middleware/cron.js
@@ -1,39 +1,60 @@
-const cron = require('node-cron');
-const logger = require('./logger');
-const User = require('../model/Users');
-const { StatusUser } = require('../config/Constans');
-
+const cron = require("node-cron");
+const logger = require("./logger");
+const User = require("../model/Users");
+const { StatusUser } = require("../config/Constans");
+const { jobModel } = require("../model/Jobs");
+const { Console } = require("winston/lib/winston/transports");
 
 const deleteUnverifiedUsers = async () => {
-    try {
-        // Tính toán thời gian hiện tại trừ đi 1 phút
-        const oneMinuteAgo = new Date(Date.now() - 5 * 60 * 1000);
+  try {
+    // Tính toán thời gian hiện tại trừ đi 1 phút
+    const oneMinuteAgo = new Date(Date.now() - 5 * 60 * 1000);
 
-        // Tìm và lưu thông tin người dùng chưa xác thực sau 1 phút
-        const unverifiedUsers = await User.userModel.find({ active: StatusUser.INACTIVE, create_at: { $lt: oneMinuteAgo } });
+    // Tìm và lưu thông tin người dùng chưa xác thực sau 1 phút
+    const unverifiedUsers = await User.userModel.find({
+      active: StatusUser.INACTIVE,
+      create_at: { $lt: oneMinuteAgo },
+    });
 
-        // Nếu có người dùng bị xóa, log thông tin của họ
-        if (unverifiedUsers.length > 0) {
-            logger.info('Deleting unverified users older than 5 minute:');
-            unverifiedUsers.forEach(user => {
-                logger.info(`User: ${user.accout_name}, Email: ${user.email}`);
-            });
+    const unverifiedJobs = await jobModel.find({
+      status: StatusUser.ACTIVE,
+      expires_at: { $lt: Date.now() },
+    });
 
-            // Xóa người dùng chưa xác thực sau 1 phút
-            await User.userModel.deleteMany({ _id: { $in: unverifiedUsers.map(user => user._id) } });
-        } else {
-            logger.info('No unverified users older than 5 minute found.');
-        }
-    } catch (error) {
-        logger.error('Error while deleting unverified users:', error);
+    if (unverifiedJobs.length > 0) {
+      console.log("Đã xóa");
+      // Xóa người dùng chưa xác thực sau 1 phút
+      await jobModel.findByIdAndUpdate({
+        _id: {
+          $in: unverifiedJobs.map((job) => job._id),
+          status: StatusUser.INACTIVE,
+        },
+      });
+    } else {
+      logger.info("No unverified users older than 5 minute found.");
     }
+
+    // Nếu có người dùng bị xóa, log thông tin của họ
+    if (unverifiedUsers.length > 0) {
+      logger.info("Deleting unverified users older than 5 minute:");
+      unverifiedUsers.forEach((user) => {
+        logger.info(`User: ${user.accout_name}, Email: ${user.email}`);
+      });
+
+      // Xóa người dùng chưa xác thực sau 1 phút
+      await User.userModel.deleteMany({
+        _id: { $in: unverifiedUsers.map((user) => user._id) },
+      });
+    } else {
+      logger.info("No unverified users older than 5 minute found.");
+    }
+  } catch (error) {
+    logger.error("Error while deleting unverified users:", error);
+  }
 };
 
-
 // Cron job chạy mỗi phút để xử lý các trường hợp khác nhau
-cron.schedule('* * * * *', async () => {
-    // Xóa người dùng chưa xác thực sau 10 phút
-    await deleteUnverifiedUsers();
-
-
+cron.schedule("* * * * *", async () => {
+  // Xóa người dùng chưa xác thực sau 10 phút
+  await deleteUnverifiedUsers();
 });
